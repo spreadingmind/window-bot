b2317d990cb629d79e1cee0ec806eafcabceb87e
Elisabeth <lisabeth.shevtsova@gmail.com>
2018-07-17 21:47:33 +0300

    tweak: update websocket server and add gitignore

---
+++ websocket_server.js
@@ -0,0 +1,33 @@
+const { createServer }  = require('http');
+const { Server } = require('ws');
+const { EventEmitter } = require('events');
+const ws_port = 8765;
+
+let event = null;
+
+const server = new createServer((req, res) => {
+  event = new EventEmitter();
+  event.on('data', (data) => {
+    res.write(data);
+    res.end();
+    event.removeListener('data', () => {});
+    event = null;
+  });
+});
+
+
+const wss = new Server({ server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    if (event) {
+      event.emit('data', message);
+      console.log('received: %s', message);
+    }
+    // send message via bot
+  });
+});
+
+server.listen(ws_port, '0.0.0.0', () => {
+  console.log(`Web socket server listening on port ${ws_port}`);
+});
