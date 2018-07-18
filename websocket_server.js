9a859cabf8ed8123d0d2dadd06cd641de05826b1
liz <lisabeth.shevtsova@gmail.com>
2018-07-18 05:50:08 +0000

    add index.html

--- websocket_server.js
+++ websocket_server.js
@@ -1,19 +1,15 @@
 require('dotenv').config();
 const { createServer }  = require('http');
 const { Server, OPEN } = require('ws');
-const { EventEmitter } = require('events');
+const fileSystem = require('fs');
+const path = require('path');
 const ws_port = process.env.WEB_SOCKET_PORT;
 
-let event = null;
-
 const server = new createServer((req, res) => {
-  event = new EventEmitter();
-  event.on('data', (data) => {
-    res.write(data);
-    res.end();
-    event.removeListener('data', () => {});
-    event = null;
-  });
+    const filePath = path.join(__dirname, 'index.html');
+    res.writeHeader(200, {"Content-Type": "text/html"});
+    const readStream = fileSystem.createReadStream(filePath);
+    readStream.pipe(res);
 });
 
 
@@ -21,10 +17,6 @@ const wss = new Server({ server });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    if (event) {
-      event.emit('data', message);
-      console.log('received: ', message);
-    }
     wss.clients.forEach(function each(client) {
         if (client !== ws && client.readyState === OPEN) {
 	    client.send(message);
